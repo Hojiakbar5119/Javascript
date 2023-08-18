26c3bb7b8fa140b17ecfc6f7e5cceb995024a6b9
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-08-18 15:52:41 +0500

    Kecha darsni organdik

---
+++ 1.js intro/script.js
@@ -0,0 +1,47 @@
+//! Salom mening ismim Hojiakbar  
+//! Javascriptda 3ta compiler yurgizish 
+// // html 
+// online cimpile 
+// nodejs
+// console.log("Salom dunyo!");
+
+// Node.js 
+
+// console.log("Salom dunyo!   \t  mening ismim Hojiakbar ");
+// console.log("Salom men Node js orqali yurgizdim!");
+
+// primitive datatype   oddiy turlari deyiladi 
+// non primitive datatype oddiy emas malumot turlari deyiladi 
+// string number bigInt null undefined sumbol  boolenan => primitive datatype
+// object  => non primitive datatype
+
+// var   let  const 
+
+
+// var  a=1;
+
+// console.log(a);
+// // variable let 
+
+// var a=1;
+// var a=2;
+
+// // const a=2;
+// // // a=1;
+// // console.log(a);
+
+
+// let a=null;
+
+// console.log(a);
+
+
+// boolean   true false 
+
+// object    funcsiya massiv object
+
+// prompt("salom sizning ismingiz nima?")
+
+
+// alert("diqqat sizning ismingiz Hojiakbarsiz")
+
