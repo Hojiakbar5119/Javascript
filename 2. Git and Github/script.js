67f77c26568927fc48b1faaf713b19e7cebc3767
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-08-20 09:58:51 +0500

    men Javascriptni o'zgartirdim

--- 2. Git and Github/script.js
+++ 2. Git and Github/script.js
@@ -1,3 +1,8 @@
 //! Git init bu initializa yuklab olish beradi
 console.log("Salom dunyo men Github o'rnatgapman ");
 
+// men javascriptda cod yozgapman 
+
+console.log("Salom men Hojiakbarman!")
+
+
