1a8afc2e2ea7add99ec361624ae7152e4aeba450
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-08-21 14:31:44 +0500

    men vazifani o'rgandim

---
+++ 3. Type conversion/script.js
@@ -0,0 +1,136 @@
+// datatype  
+
+//!string va number boolean 
+
+// let var
+
+//! string number 
+ 
+// let a = "12"
+// console.log(Number(a))
+
+
+// let a= "12a"
+
+// console.log(+a);  //Not a number
+
+
+// console.log(parseInt(a));
+
+// a = +"12"
+
+// console.log(a);
+
+// console.log(Boolean(""));
+
+// let a= "12";
+// let b= 14;
+
+// // console.log(b+ +a);
+// // console.log(b+ Number(a));
+
+// console.log(parseFloat(b/a));
+
+
+// incremment decremetn
+
+//? increment
+// postfix
+// let a=2;
+
+// console.log(a++);
+
+
+// // prefix
+// let b=3
+// console.log(++b);
+
+
+//decremnt
+
+// let a=2;
+// a--
+
+// console.log(a);
+
+
+// let a= 2**4;
+// console.log(a);
+
+// % qoldiqli bo'lish deyiladi
+
+// == noqatiy tengsizlik deyiladi 
+
+// console.log(""===" ");
+
+// console.log(5>4 ); // true
+
+// console.log("apple" > "apple");  bir biriga obyekt bo'lganligi uchun false qiymati qaytarar ekan
+
+
+// console.log(undefined == null); //false
+
+
+// console.log("\n0\n"); //false
+
+// console.log(null == +"\n0\n"); //false
+
+// let a=1, b=1;
+// let c=++a; //2
+// let d = b++ //1
+
+
+
+// let a =2;
+
+// // a= a *2
+
+// let x= 1 + (a*=2) // 5 
+
+// console.log(x);
+
+
+
+// console.log("" + 1 + 0 ); // 10
+// console.log("1" +0); // 10
+
+// console.log("" -1 +0 ); //-1
+
+
+// console.log(true + false); //1
+
+// console.log(6/"3"); //2
+
+// console.log("2" * "3"); //6 
+// console.log("2" / "3"); //number 
+// console.log("2" - "3"); //number 
+
+
+
+
+// console.log(4+5+"px"); //9px
+
+// console.log("$" + 4 +5); //$45
+
+// console.log("4" -2); //2
+// console.log("4" *2); //8
+// console.log("4" /2); //2
+// console.log("4" %2); //0
+
+
+
+// console.log("4px" *2); // nan
+
+
+// console.log(  "  -9  " + 5); //-95
+// console.log(  "  -9  " - 5); //-14
+
+// console.log(null +1); //1
+
+// let a=null
+// console.log(null -1); //-1
+// console.log(undefined +1); //1
+
+// console.log(" \t \n" -2); //Nan
+console.log(" \t \n"); 
+
