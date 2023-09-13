ee0a3f1c4f9d0b81d5109123e26dfd7fb9f48c16
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-09-13 17:05:30 +0500

    array advanced

--- 8. Array/script.js
+++ 8. Array/script.js
@@ -71,19 +71,147 @@
 //   const arr = [3, 4, 3, 2, 3, 1, 3, 3];
 //   console.log(dominator(arr)); // Output: 3
 
-function dominator(arr) {
-  const count = {};
-  const n = arr.length;
-  for (let i = 0; i < n; i++) {
-    const num = arr[i];
-    if (count[num] === undefined) {
-      count[num] = 1;
-    } else {
-      count[num]++;
-    }
-    if (count[num] > n / 2) {
-      return num;
-    }
-  }
-  return -1;
-}
+// function dominator(arr) {
+//   const count = {};
+//   const n = arr.length;
+//   for (let i = 0; i < n; i++) {
+//     const num = arr[i];
+//     if (count[num] === undefined) {
+//       count[num] = 1;
+//     } else {
+//       count[num]++;
+//     }
+//     if (count[num] > n / 2) {
+//       return num;
+//     }
+//   }
+//   return -1;
+// }
+
+
+
+// arr[2, 4, undefined, null ,"webbrain"]
+
+
+// const massiv = []
+// const massiv2 = new Array();
+
+// // console.log(typeof(massiv));
+// // console.log(typeof(massiv2));
+
+
+// // [1,23,4,5,2,4,5,]
+// console.log(massiv2.length);
+
+
+
+
+// const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // raqamlar massivi
+
+// console.log(typeof(numbers));
+// const fruits = ['banana', 'orange', 'mango', 'lemon'] // satrlar massivi, mevalar
+// const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // satrlar massivi, sabzavotlar 
+// // const animalProducts = ['milk', 'meat', 'butter', 'yoghurt'] // satrlar massivi, mahsulotlar
+// // const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // web-texnologiyalar massivi
+// // const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'] // satrlar massivi, mamlakatlar
+
+
+
+// const massiv = new Array(4)
+
+// console.log(massiv);
+
+// let ar = [12, 3, 4, "hey", true, function () {}, null, undefined];
+
+// console.log(ar[0]);
+// console.log(ar.at(-3));
+
+// let ar = new Array("4"); // array length
+
+// // console.log(ar[100]);
+// console.log(ar.length);
+
+// let ar1 = [1, 2, 3, 4];
+// let ar2 = [5, 6, 7];
+
+// // console.log(ar1.toString() + ar2.toString());
+// console.log(ar1.join());
+
+// console.log(typeof(ar1));
+// console.log(Array.isArray([1]));
+
+// let ar = [];
+
+// console.log(ar == 0);
+// console.log(ar == "0");
+
+// let user = ["Abdulloh", "Muhammad", "Sardorbek", "Shohruh"];
+
+// for (let i = 1; i < user.length; i=i+2) {
+//   console.log(user[i].toLowerCase());
+// }
+
+// console.log(user[0].toUpperCase());
+
+// for (index in user) {
+//   console.log(index);
+// }  //!  indexlarni ko'rib bera oladi adashmaylik in bo'lsa bo'lsa 
+
+// for(value of user){
+//   console.log(value);
+// }
+
+// let ar = [1, 2, 3];
+
+// ar.pop();
+// ar.push(4);
+
+// console.log(ar[1]);
+
+// ar.unshift(0);
+// ar.shift();
+
+// console.log(ar[1]);
+
+// let ar = [1, 2, 3, 4, 5];
+// console.log(ar.slice(1, 3));
+// console.log(ar);
+
+// let ar = [1, 2, 3, 4, 5]; // insertion, remove
+
+// console.log(ar.splice(2, 1, "Najmiddin tushundingizmi?"));
+// console.log(ar);
+
+// let ar1 = [1, 2, 3];
+// let ar2 = [4, 5, 6];
+// console.log(ar1.concat(ar2, ar1));
+
+// console.log(ar1.indexOf(1, 3));
+// console.log(ar1.lastIndexOf(1, 3));
+
+// let str = "1,23,3,4";
+
+// console.log(str.split(","));
+
+// console.log(ar1.join("").split(","));
+
+// geCacl(ar){
+
+// }
+
+// let ar1 = [1, 2, 3, 4];
+// let ar2 = ["webbrain", "academy"];
+// res = {
+//   w: 1,
+//   e: 2,
+//   b: 2,
+//   r: 1,
+//   n: 1,
+//   c: 1,
+//   a: 3,
+//   d: 1,
+//   m: 1,
+// };
+
+// // ar1.reverse();
+// console.log(ar1.includes(11));
