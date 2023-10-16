45a07970b2d6cb6a3fef0a5758c8d63ae8cf4586
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-10-16 09:33:43 +0500

    class users

---
+++ 17.class/script.js
@@ -0,0 +1,14 @@
+class Users{
+    age =0;
+    // console.log('');
+     FullName() {
+        console.log(++this.age);
+        // console.log(`Hi mening ismim`);
+    }
+}
+let usr1 = new Users()
+let usr2 = new Users()
+
+
+usr1.FullName()
+usr1.FullName()
