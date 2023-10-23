70c078f039a950d7c5b9645bb2ada90e2107e84e
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-10-23 12:46:20 +0500

    call user orqali chaqrishni o'rganish

--- 23.proxy/script.js
+++ 23.proxy/script.js
@@ -1,23 +1,25 @@
-let user={
-    name: 'Webbrain',
-    _stir:'1234',
-}
+// let user={
+//     name: 'Webbrain',
+//     _stir:'1234',
+// }
 
-let range={
-    from:1,
-    to:10,
-}
+//? Obyektdan  1dan 10gacha bo'lgan mmalumotlarni bor yoki yo'qligini tekshirish 
 
-let proxy = new Proxy(range,{
-    has(target,prop){
-        return target.from<=prop && target.to>=prop
-    }
+// let range={
+//     from:1,
+//     to:10,
+// }
+
+// let proxy = new Proxy(range,{
+//     has(target,prop){
+//         return target.from<=prop && target.to>=prop
+//     }
     
-})
+// })
 
-range.to = 10
+// range.to = 10
 
-console.log(99 in proxy);
+// console.log(99 in proxy);
 
 // let proxy = new Proxy(user, {
 //     // get(target,props){
@@ -42,3 +44,47 @@ console.log(99 in proxy);
 //     // console.log(user[vl]);
 //     console.log(vl);
 // }
+
+//? get set method orqali qoshish yani reflect qilish 
+// const target ={
+//     name: 'Hojiakbar',
+//     age:23, 
+// }
+
+// const handler = {
+//     set: function(target,prop, value){
+//         console.log(`Men Set metod orqali ${prop} va ${value} qo'shdim`);
+//         return target[prop] =value
+//     }
+// }
+
+// const proxyes = new Proxy(target,handler)
+
+// proxyes.job = 'Frontend mentor'
+
+// console.log(proxyes);
+// console.log(target);
+
+//? call user orqali chaqirish qanaqaligini bilib olamiz 
+
+// const user ={
+//     name: "hojiakbar",
+//     _pw:'1234',
+//     get(){
+//         console.log(this.name, this._pw);
+//     }
+// }
+
+//  console.log(user.get);
+
+// const data = user.get
+// data.call(user)
+// data
+
+
+function get(){
+    console.log(this);
+}
+console.log(this);
+
+get()
