f7c9ba91e9a919532462e1046ef7e1a282294157
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-10-30 14:15:43 +0500

    DOM

---
+++ 24. Dom learning/script.js
@@ -0,0 +1,8 @@
+const by = document.getElementsByTagName('body')
+// console.log(by);
+
+const tag = document.getElementsByTagName('h1')
+const tags = document.querySelectorAll('h1')
+const sele = document.querySelector('li')
+sele.innerText = "<h1>Salom dunyo </h1>"
+console.log(sele);
